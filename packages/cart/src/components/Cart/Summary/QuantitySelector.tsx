b8e0e0d7bb0f5bd4e58945b364f012775d794411
manju <manju@ezcontacts.com>
2023-07-10 07:48:10 +0530

    completed contact lense design and coupon code funcationality chnages

--- packages/cart/src/components/Cart/Summary/QuantitySelector.tsx
+++ packages/cart/src/components/Cart/Summary/QuantitySelector.tsx
@@ -3,6 +3,7 @@ import { FC } from "react"
 import { useTranslation } from "react-i18next"
 
 import { InputSpinner } from "#components/atoms/InputSpinner"
+import { useSettings } from "#components/SettingsProvider"
 
 type Props = {
   readonly?: boolean
@@ -23,6 +24,48 @@ export const QuantitySelector: FC<Props> = () => {
           />
         )}
       </LineItemQuantity>
+      <Errors
+        resource="line_items"
+        className="absolute top-[100%] block text-xs text-red-400"
+        messages={[
+          {
+            code: "VALIDATION_ERROR",
+            resource: "line_items",
+            field: "quantity",
+            message: t("general.quantityNotAvailable"),
+          },
+        ]}
+      />
+    </div>
+  )
+}
+
+export const NewQuantitySelector = ({
+  quantity,
+  cartItem,
+  updateItemQuantity,
+}: any) => {
+  const { settings } = useSettings()
+  if (!settings || !settings.isValid) {
+    return null
+  }
+  const { id } = cartItem
+  const { t } = useTranslation()
+
+  const onSelectChnage = (event: any, id: any) => {
+    if (event.target.value) {
+      updateItemQuantity(event.target.value, id)
+    }
+  }
+
+  return (
+    <div className="relative w-full">
+    <InputSpinner
+        data-test-id="quantity-selector"
+        quantity={quantity}
+        handleChange={(event) => onSelectChnage(event, id)}
+        debounceMs={600}
+      />
       <Errors
         resource="line_items"
         className="absolute top-[100%] block text-xs text-red-400"

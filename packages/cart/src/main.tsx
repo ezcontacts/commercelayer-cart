ca74258a7dd38f8bda37fc84bbd4f874cfdf125f
manju <manju@ezcontacts.com>
2023-12-18 12:08:23 +0530

    done ip confiration chnages

--- packages/cart/src/main.tsx
+++ packages/cart/src/main.tsx
@@ -1,6 +1,6 @@
+
 import React from "react"
 import { createRoot } from "react-dom/client"
-
 import "#styles/globals.css"
 import "#styles/cart.css"
 import "#styles/footer.css"
@@ -9,29 +9,44 @@ import App from "./App"
 import { OptimizelyProvider } from "@optimizely/react-sdk"
 import optimizely from "#utils/optimizely"
 
-const user = {
-  id: "user123",
-  attributes: {
-    logged_in: "true",
-    server_ip: "172.20.21.13",
-    country: "Germany",
-    city: "Frankfurt am Main",
-    region: "Hesse",
-    country_code: "IN",
-    postal_code: "60313",
-    continent_code: "",
-    os: "Windows",
-    device: "Desktop",
-    browser: "MSIE",
-    browser_version: "9.0",
-    Guest: "1",
-  },
+const getIp = async () => {
+  const response = await fetch("https://api.ipify.org?format=json")
+  const data = await response.json()
+  const userResponse = await fetch(`https://ipapi.co/${data.ip}/json/`)
+  const userData = await userResponse.json()
+  return userData
 }
 
-createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <OptimizelyProvider optimizely={optimizely} user={user}>
-      <App />
-    </OptimizelyProvider>
-  </React.StrictMode>
-)
+const createUser = async () => {
+  const userLocation = await getIp()
+  return {
+    id: "user123",
+    attributes: {
+      logged_in: "true",
+      server_ip: userLocation.ip,
+      country: userLocation.country,
+      city: userLocation.city,
+      region:  userLocation.region,
+      country_code: userLocation.country_code,
+      postal_code: userLocation.postal,
+      continent_code: userLocation.continent_code,
+      os: "windows",
+      device: "desktop",
+     Guest: "1",
+    },
+  }
+}
+
+const renderApp = async () => {
+  const user = await createUser()
+
+  createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+      <OptimizelyProvider optimizely={optimizely} user={user}>
+        <App />
+      </OptimizelyProvider>
+    </React.StrictMode>
+  )
+}
+
+renderApp()

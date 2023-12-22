530e5de457bb14d44e44a92e8e630b1bd2aa4d37
manju <manju@ezcontacts.com>
2023-12-22 07:45:23 +0530

    done 404 redirection chnages changes

--- packages/cart/src/App.tsx
+++ packages/cart/src/App.tsx
@@ -8,9 +8,44 @@ import CartPage from "./pages/CartPage"
 import ErrorPage from "./pages/ErrorPage"
 import { getVisitorId } from "#utils/getLogedinStatus"
 
+const getOrderStatus = async () => {
+  var urlString = window?.location?.href
+  var url = new URL(urlString)
+  var queryParams = url?.searchParams
+  var accessToken = queryParams?.get("accessToken")
+  const orderId = window?.location?.href.split("/").pop()
+  const response = await fetch(
+    `${process.env.REACT_APP_PUBLIC_CL_URL_PATH}/api/orders/${orderId}`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/vnd.api+json",
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/vnd.api+json",
+      },
+    }
+  )
+  const data = await response.json()
+
+  return data?.data?.attributes?.status
+}
+
 function App() {
   const visitoId = getVisitorId()
+  const [orderstatus, setOderstatus] = React.useState("")
+
   useEffect(() => {
+    const fetchOrderStatus = async () => {
+      const orderStatus = await getOrderStatus()
+      setOderstatus(orderStatus)
+      console.log("orderstatus-latest", orderstatus)
+      if (orderStatus === "placed") {
+        window.location.href = `${process.env.REACT_APP_PUBLIC_ODOO_PATH}`
+        return false
+      }
+    }
+
+    fetchOrderStatus()
     fetch("https://api.ipify.org?format=json")
       .then((response) => response.json())
       .then((data) => {
@@ -18,7 +53,7 @@ function App() {
         localStorage.setItem("IP", userIP)
       })
       .catch((error) => console.error("Error fetching user IP:", error))
-  }, [])
+  }, [orderstatus])
 
   const basePath =
     import.meta.env.PUBLIC_PROJECT_PATH != null
@@ -32,6 +67,7 @@ function App() {
       is_logged_in: true,
     },
   })
+
   return (
     <>
       <OptimizelyProvider optimizely={optimizely}>
@@ -39,11 +75,11 @@ function App() {
           <EmbeddedCapabilities.IframeResizerInit />
           <Router base={basePath}>
             <Switch>
-              <Route path={"/:orderId"}>
-                <CartPage />
-              </Route>
               <Route path={"/404"}>
                 <ErrorPage />
+              </Route>
+              <Route path={"/:orderId"}>
+                <CartPage />
               </Route>
               <Route>
                 <ErrorPage />

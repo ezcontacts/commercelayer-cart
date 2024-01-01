e362df6ab6d549ed2d04d5cb6f7b0f2d63844c10
manju <manju@ezcontacts.com>
2024-01-01 17:18:16 +0530

    integrated visitor id in cl logs

--- packages/cart/src/utils/cllogs.ts
+++ packages/cart/src/utils/cllogs.ts
@@ -1,4 +1,8 @@
 export const saveUserActivitylogData = async (request: any) => {
+  var urlString = window?.location?.href
+  var url = new URL(urlString)
+  var queryParams = url?.searchParams
+  var visitorId = queryParams?.get("ezref")
   const IP = localStorage.getItem("IP")
   let requestBody = {
     requested_method: request.requested_method,
@@ -6,15 +10,19 @@ export const saveUserActivitylogData = async (request: any) => {
     requested_data: request.requested_data,
     response_data: request.response_data,
     ip_address: IP || "",
+    visitor_id: visitorId || "",
   }
 
-  return fetch(`${process.env.REACT_APP_PUBLIC_ODOO_PATH}/cl/saveClUserTempLogs`, {
-    headers: {
-      Accept: "application/json",
-    },
-    method: "POST",
-    body: JSON.stringify(requestBody),
-  })
+  return fetch(
+    `${process.env.REACT_APP_PUBLIC_ODOO_PATH}/cl/saveClUserTempLogs`,
+    {
+      headers: {
+        Accept: "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(requestBody),
+    }
+  )
     .then((response) => response.json())
     .then((result) => {
       if (result?.success) {

846909e46938c34b4f6c682cb73c6a6a074ab4f3
manju <manju@ezcontacts.com>
2024-02-08 14:28:04 +0530

    TR-620 added optimizly data file

--- packages/cart/src/utils/optimizely.ts
+++ packages/cart/src/utils/optimizely.ts
@@ -1,12 +1,11 @@
 import { createInstance } from "@optimizely/react-sdk"
 
 export const optimizely = createInstance({
-  sdkKey: "QovWN9SQwD19hPLTsZr9f",
+  sdkKey: process.env.REACT_APP_OPTIMIZELY_KEY,
   datafileOptions: {
     updateInterval: 600000,
     autoUpdate: true,
-    urlTemplate:
-      "https://cdn.optimizely.com/datafiles/QovWN9SQwD19hPLTsZr9f.json",
+    urlTemplate: process.env.REACT_APP_OPTIMIZELY_URL,
   },
 })
 export default optimizely

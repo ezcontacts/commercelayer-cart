733b4b38b016f6f8033285ca0b58bd92a0d6ed23
manju <manju@ezcontacts.com>
2024-02-09 14:22:13 +0530

    devolpment testing for optimiszly

--- packages/cart/src/utils/logClMetrics.ts
+++ packages/cart/src/utils/logClMetrics.ts
@@ -2,10 +2,12 @@ import { OptimizelyContext } from "@optimizely/react-sdk"
 import { useContext, useState, useEffect } from "react"
 
 const useLogMetricsData = () => {
-  const { optimizely } = useContext(OptimizelyContext)
+  const { optimizely } = useContext(OptimizelyContext) as any;
+  debugger;
   const [decision, setDecision] = useState(false)
   useEffect(() => {
     if (optimizely) {
+      debugger
       const result = optimizely.decide("commerce_layer")
       if (result) {
         setDecision(result.enabled)
@@ -14,7 +16,9 @@ const useLogMetricsData = () => {
   }, [optimizely])
 
   const logMetrics = (event: string) => {
+    console.log("test123w", optimizely?.initialConfig)
    console.log("logMetrics", event)
+   debugger
     if (decision) {
       console.log("decision", decision)
       // No need to use onReady before track

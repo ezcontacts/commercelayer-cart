15c5da225c67aa8f51efa067f74514b6d7adad0f
manju <manju@ezcontacts.com>
2024-02-14 08:24:26 +0530

    rebase header chnages

--- packages/cart/src/components/PageHeader.tsx
+++ packages/cart/src/components/PageHeader.tsx
@@ -1,63 +1,11 @@
-// import cn from "classnames"
-// import { FC, ReactNode } from "react"
-
-// // import { CompanyLogo } from "#components/CompanyLogo"
-// import { Ezbanner } from "./common/ezbanner"
-
-// import { SkeletonItem } from "#components/Skeleton/Item"
-// import { isEmbedded } from "#utils/isEmbedded"
-
-// type Props = {
-//   /**
-//    * Content to be displayed as page header, underneath the company logo.
-//    */
-//   children?: ReactNode
-//   /**
-//    * When this is `true` a Skeleton UI will be displayed instead of `children`
-//    */
-//   isLoading?: boolean
-//   userEmail?: string
-// }
-
-// export const PageHeader: FC<Props> = ({ isLoading, children, userEmail }) => {
-//   if (isEmbedded()) {
-//     // we don't need page header when app is working in embedded mode
-//     return null
-//   }
-
-//   return (
-//     <div
-//       className={cn({
-//         "animate-pulse": isLoading,
-//       })}
-//       data-test-id="cart-header"
-//     >
-//       {/* <div className="py-8 border-b border-b-gray-200">
-//         <CompanyLogo />
-//       </div> */}
-//       <div className="flex justify-between items-center pb-6">
-//         {isLoading ? (
-//           <>
-//             <SkeletonItem className="h-[54px] mt-6 full-width" />
-//             {/* <SkeletonItem className="w-1/12 h-[36px]" />
-//             <SkeletonItem className="w-1/12 h-[20px]" /> */}
-//           </>
-//         ) : (
-//           children
-//         )}
-//       </div>
-//       <Ezbanner userEmail={userEmail} />
-//     </div>
-//   )
-// }
-
 import cn from "classnames"
 import { FC, ReactNode } from "react"
 
-import { CompanyLogo } from "#components/CompanyLogo"
+// import { CompanyLogo } from "#components/CompanyLogo"
+import { Ezbanner } from "./common/ezbanner"
+
 import { SkeletonItem } from "#components/Skeleton/Item"
 import { isEmbedded } from "#utils/isEmbedded"
-import { Ezbanner } from "./common/ezbanner"
 
 type Props = {
   /**
@@ -84,14 +32,15 @@ export const PageHeader: FC<Props> = ({ isLoading, children, userEmail }) => {
       })}
       data-test-id="cart-header"
     >
-      <div className="py-8 border-b border-b-gray-200">
+      {/* <div className="py-8 border-b border-b-gray-200">
         <CompanyLogo />
-      </div>
+      </div> */}
       <div className="flex justify-between items-center pb-6">
         {isLoading ? (
           <>
-            <SkeletonItem className="w-1/12 h-[36px]" />
-            <SkeletonItem className="w-1/12 h-[20px]" />
+            <SkeletonItem className="h-[54px] mt-6 full-width" />
+            {/* <SkeletonItem className="w-1/12 h-[36px]" />
+            <SkeletonItem className="w-1/12 h-[20px]" /> */}
           </>
         ) : (
           children
@@ -101,3 +50,54 @@ export const PageHeader: FC<Props> = ({ isLoading, children, userEmail }) => {
     </div>
   )
 }
+
+// import cn from "classnames"
+// import { FC, ReactNode } from "react"
+
+// import { CompanyLogo } from "#components/CompanyLogo"
+// import { SkeletonItem } from "#components/Skeleton/Item"
+// import { isEmbedded } from "#utils/isEmbedded"
+// import { Ezbanner } from "./common/ezbanner"
+
+// type Props = {
+//   /**
+//    * Content to be displayed as page header, underneath the company logo.
+//    */
+//   children?: ReactNode
+//   /**
+//    * When this is `true` a Skeleton UI will be displayed instead of `children`
+//    */
+//   isLoading?: boolean
+//   userEmail?: string
+// }
+
+// export const PageHeader: FC<Props> = ({ isLoading, children, userEmail }) => {
+//   if (isEmbedded()) {
+//     // we don't need page header when app is working in embedded mode
+//     return null
+//   }
+
+//   return (
+//     <div
+//       className={cn({
+//         "animate-pulse": isLoading,
+//       })}
+//       data-test-id="cart-header"
+//     >
+//       <div className="py-8 border-b border-b-gray-200">
+//         <CompanyLogo />
+//       </div>
+//       <div className="flex justify-between items-center pb-6">
+//         {isLoading ? (
+//           <>
+//             <SkeletonItem className="w-1/12 h-[36px]" />
+//             <SkeletonItem className="w-1/12 h-[20px]" />
+//           </>
+//         ) : (
+//           children
+//         )}
+//       </div>
+//       <Ezbanner userEmail={userEmail} />
+//     </div>
+//   )
+// }
